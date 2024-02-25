4a7582f8704dc5519446fac274c6d7ec98bd0eca
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-02-25 15:34:39 +0200

    Basic checkout stripe-hosted page

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express');
-
-const app = express();
 const PORT = 3000;
+const app = express();
+const MY_DOMAIN = `http://localhost:${PORT}`;
+
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });
 });
 
-app.get('/pay', (req, res) => {
-    res.json({
-        message: 'Payment Complete!',
+app.get('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                // Price_id for product defined in stripe products
+                price: `price_1OnhhdG6mitVHZhyzcDacAyT`,
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${MY_DOMAIN}/success`,
+        cancel_url: `${MY_DOMAIN}/cancel`,
     });
+    res.redirect(303, session.url);
 });
 
 app.listen(PORT, () => {
